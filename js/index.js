38b084721d29cd254e99a7fb592caf010d4d3297
brunofruet <brunofruet17@gmail.com>
2021-12-12 17:20:26 -0300

    Agregar Ciudad Funciona

--- js/index.js
+++ js/index.js
@@ -1,10 +1,34 @@
 // let citySaved = localStorage.getItem('cities');
 let inputCity = document.getElementById("cityAdd");
+var citiesSaved = {
+    1: "rosario",
+    2: "buenos aires",
+    3: "corrientes"
+};
+function initDataCities(){
+    for (var [key, value] of Object.entries(citiesSaved)){
+        localStorage.setItem(key, value)
+    }
+}
+initDataCities();
+let size = Object.keys(citiesSaved).length;
 
 function validateCity(){
-
+let validate = true;
+    for (var [key, value] of Object.entries(citiesSaved)){
+        if (value.toString() == inputCity.value.toLowerCase()){
+            console.log("ya existe");
+            validate = false;
+        }
+    }
+    if (validate){
+        addCity(inputCity.value.toLowerCase());
+        size = Object.keys(citiesSaved).length;
+    }
 }
 
-function addCity(newcity){
-    localStorage.setItem("cities", newcity)
+function addCity(newCity){
+    let newCityKey = size + 1; 
+    citiesSaved[newCityKey] = newCity;
+    localStorage.setItem(newCityKey, newCity)
 }
